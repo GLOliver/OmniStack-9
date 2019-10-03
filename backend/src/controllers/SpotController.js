59f3b9396ff0335406cb8f9b81801cc62ceb3f00
GLOliver <gui.lopes11@gmail.com>
2019-10-03 17:18:52 -0300

    Aula 01 (Incompleto) - 1:08:40

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,10 @@
+const Spot = require('../models/Spot');
+
+module.exports = {
+    async store(req, res) {
+        const { filename } = req.file;
+        const { company, techs, price } = req.body
+ 
+        return res.json({ ok:true })
+    }
+};
